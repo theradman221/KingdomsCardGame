f79855e3e66b18534ee7e5d73712506ba20d3e73
unknown <D_homer@live.com>
2021-03-29 16:25:15 -0600

    main menu wip

--- bin/pyfiles/menus/mainmenu.py
+++ bin/pyfiles/menus/mainmenu.py
@@ -3,10 +3,10 @@ import sys
 
 from pygame.locals import *
 
+
 mainClock = pygame.time.Clock()
 pygame.init()
 pygame.display.set_caption('Kingdoms_Main_Menu')
-WIN = pygame.display.set_mode((500,500), 0,32)
 font = pygame.font.SysFont('Corbel', 20)
 button_play = pygame.Rect(50, 100, 200, 50)
 button_deck = pygame.Rect(50, 200, 200, 50)
@@ -21,6 +21,7 @@ def menu_draw_text(text, font, color, surface, x, y):
 
 
 def draw_window():
+    WIN = pygame.display.set_mode((500, 500), 0, 32)
     WIN.fill((100, 100, 100))
     menu_draw_text('Main Menu', pygame.font.SysFont('Corbel', 35), (255, 255, 255), WIN, 170, 20)
     pygame.draw.rect(WIN, (255, 255, 255), button_play)
@@ -39,9 +40,6 @@ def main_menu():
         draw_window()
 
         mx, my = pygame.mouse.get_pos()
-        pygame.draw.rect(WIN, (255, 255, 255), button_play)
-        pygame.draw.rect(WIN, (255, 255, 255), button_deck)
-        pygame.draw.rect(WIN, (255, 255, 255), button_quit)
 
         if button_play.collidepoint((mx,my)):
             if click:
