90983b553f27178d7432faa948f3fdd97d7ac8a2
unknown <D_homer@live.com>
2021-04-20 14:12:02 -0600

    working on gameboard

--- bin/pyfiles/menus/gameboard.py
+++ bin/pyfiles/menus/gameboard.py
@@ -1,17 +1,35 @@
 import pygame
 import sys
+import os
+
+
+WIDTH, HEIGHT = 1920,1080
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Gameboard")
+GRAY = (75,75,75)
+FPS = 60
+print(os.getcwd())
+backGround_Image = pygame.image.load(os.getcwd() + '\gameboard\gameboard.jpg')
+backGround_Image = pygame.transform.scale(backGround_Image, (1520, 1080))
+
+def draw_window():
+    WIN.fill(GRAY)
+    WIN.blit(backGround_Image, (200, 0))
+    pygame.display.update()
 
 def bg():
-    pygame.init()
-    win = pygame.display.set_mode((900,900))
-    bg_img = pygame.image.load()
-    bg = pygame.transform.scale(bg_img,(900,900))
-
-
-
-    run =  True
+    clock = pygame.time.Clock()
+    run = True
     while run:
+        clock.tick(FPS)
         for event in pygame.event.get():
-            pygame.display.update()
+            if event.type == pygame.QUIT:
+                run = False
+        draw_window()
 
-bg()
+    pygame.quit()
+
+
+
+if __name__ == "__main__":
+    bg()
